7e5c32dc57abe01c9565ef8601a5538d807ff8ea
Michal Kwiatkowski <kfiatos@gmail.com>
2015-07-17 10:33:05 +0200

    Initial commit

---
+++ src/user.php
@@ -0,0 +1,21 @@
+<?php
+
+class User{
+  /*
+  CREATE TABLE Users(
+          id int AUTO_INCREMENT,
+          nick VARCHAR (255) UNIQUE NOT NULL,
+          hashed_password  VARCHAR(60) NOT NULL,
+          description text,
+          PRIMARY KEY(id)
+  );
+   */
+
+
+
+  private $id;
+  private $name;
+  private $desc;
+}
+
+?>
